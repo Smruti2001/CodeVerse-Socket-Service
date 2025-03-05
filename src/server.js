de6f38a5a2578414fa0aa8bb3160e6ef8b12b2f9
Smruti Mahapatra <getsmruti03@gmail.com>
2025-03-06 02:48:39 +0530

    Added the code for emitting an event for accepting the response from submission service

--- src/server.js
+++ src/server.js
@@ -1,18 +1,21 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 const Redis = require('ioredis');
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer, { 
+const io = new Server(httpServer, {
     cors: {
         origin: ["http://localhost:5500"],
         methods: ["GET", "POST"]
-      }
- });
+    }
+});
 
-const redisCache = new Redis(); 
+const redisCache = new Redis();
+
+app.use(bodyParser.json());
 
 io.on("connection", (socket) => {
     console.log(`Connected with the client having ID: ${socket.id}`);
@@ -26,6 +29,24 @@ io.on("connection", (socket) => {
         socket.emit('socketId', JSON.stringify(socketId));
     })
 });
+
+
+app.post('/sendPayload', async (req, res) => {
+    const { userId, payload } = req.body;
+
+    if (!userId || !payload) {
+        res.status(400).send('Invalid Request');
+    }
+
+    const socketId = await redisCache.get(userId);
+
+    if(socketId) {
+        io.to(socketId).emit('submissionPayloadResponse', payload);
+        res.send('Payload sent successdully');
+    } else {
+        res.status(400).send('SocketId not found');
+    }
+})
 
 httpServer.listen(3600, () => {
     console.log(`Server started listening on PORT: 3600`);
